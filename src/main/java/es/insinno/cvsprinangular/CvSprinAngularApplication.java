717d42aacabc64b7bc5e5dc21bbd13ab9fc71217
AntonioRodenasJurado <antonio.rodenas@insinno.es>
2025-03-04 09:30:51 +0100

    initialize Angular application with basic structure and configuration

---
+++ src/main/java/es/insinno/cvsprinangular/CvSprinAngularApplication.java
@@ -0,0 +1,13 @@
+package es.insinno.cvsprinangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvSprinAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvSprinAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/insinno/cvsprinangular/CvSprinAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package es.insinno.cvsprinangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvSprinAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
