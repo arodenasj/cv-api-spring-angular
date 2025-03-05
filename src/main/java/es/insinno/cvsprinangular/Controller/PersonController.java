fbd754bb727f58e744be467a3fa081694d8d12f9
AntonioRodenasJurado <antonio.rodenas@insinno.es>
2025-03-05 10:07:19 +0100

    configure H2 database and update Person entity and controller for improved functionality
    Signed-off-by: AntonioRodenasJurado <antonio.rodenas@insinno.es>

--- src/main/java/es/insinno/cvsprinangular/Controller/PersonController.java
+++ src/main/java/es/insinno/cvsprinangular/Controller/PersonController.java
@@ -1,30 +1,46 @@
 package es.insinno.cvsprinangular.Controller;
 
+    import es.insinno.cvsprinangular.Entity.Person;
+    import es.insinno.cvsprinangular.Repository.PersonRepository;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import es.insinno.cvsprinangular.Entity.Person;
-import es.insinno.cvsprinangular.Repository.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+    import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/person")
-public class PersonController {
+    @RestController
+    @RequestMapping("/api/person")
+    public class PersonController {
 
-    @Autowired
-    private final PersonRepository personRepository;
+        @Autowired
+        private final PersonRepository personRepository;
 
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+        public PersonController(PersonRepository personRepository) {
+            this.personRepository = personRepository;
+        }
+
+        @GetMapping("/view/{id}")
+        public ResponseEntity<Person> viewPersonById(@PathVariable Long id) {
+            Optional<Person> person = personRepository.findById(id);
+            if (person.isPresent()) {
+                return new ResponseEntity<>(person.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+
+        @PostMapping("/add")
+        public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+            try {
+                if (person.getName() == null || person.getSurname() == null) {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+                Person savedPerson = personRepository.save(person);
+                return new ResponseEntity<>(savedPerson, HttpStatus.CREATED);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
     }
-
-  @GetMapping("/view")
-    public Person viewPersonbyId() {
-        return personRepository.findById(1L).orElse(null);
-    }
-
-    @PostMapping("/add")
-    public Person addPerson(@RequestBody Person person) {
-        return personRepository.save(person);
-    }
-
-}
--- src/main/java/es/insinno/cvsprinangular/Entity/Person.java
+++ src/main/java/es/insinno/cvsprinangular/Entity/Person.java
@@ -1,120 +1,115 @@
 package es.insinno.cvsprinangular.Entity;
 
+        import jakarta.persistence.*;
 
-import jakarta.persistence.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+        @Entity
+        public class Person {
+            @Id
+            @GeneratedValue(strategy = GenerationType.AUTO)
+            @Column(nullable = false)
+            private Long id;
 
-@Entity
+            private String name;
+            private String surname;
+            private String email;
+            private String phone;
+            private String address;
+            private String city;
+            private String country;
+            private String image;
 
-public class Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(nullable = false)
-    @JdbcTypeCode(SqlTypes.LONGVARCHAR)
-    private Long id;
+            public Person() {
+            }
 
-    private String name;
-    private String surname;
-    private String email;
-    private String phone;
-    private String address;
-    private String city;
-    private String country;
-    private String image;
+            public Person(String name, String surname) {
+                this.name = name;
+                this.surname = surname;
+            }
 
-    public Person() {
-        
-    }
+            public Long getId() {
+                return id;
+            }
 
-    public Long getId() {
-        return id;
-    }
+            public void setId(Long id) {
+                this.id = id;
+            }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+            public String getName() {
+                return name;
+            }
 
-    public Person(String name, String surname) {
-this.name = name;
-this.surname = surname;
-}
+            public void setName(String name) {
+                this.name = name;
+            }
 
-    public String getName() {
-        return name;
-    }
+            public String getSurname() {
+                return surname;
+            }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+            public void setSurname(String surname) {
+                this.surname = surname;
+            }
 
-    public String getSurname() {
-        return surname;
-    }
+            public String getEmail() {
+                return email;
+            }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+            public void setEmail(String email) {
+                this.email = email;
+            }
 
-    public String getEmail() {
-        return email;
-    }
+            public String getPhone() {
+                return phone;
+            }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+            public void setPhone(String phone) {
+                this.phone = phone;
+            }
 
-    public String getPhone() {
-        return phone;
-    }
+            public String getAddress() {
+                return address;
+            }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+            public void setAddress(String address) {
+                this.address = address;
+            }
 
-    public String getAddress() {
-        return address;
-    }
+            public String getCity() {
+                return city;
+            }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+            public void setCity(String city) {
+                this.city = city;
+            }
 
-    public String getCity() {
-        return city;
-    }
+            public String getCountry() {
+                return country;
+            }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+            public void setCountry(String country) {
+                this.country = country;
+            }
 
-    public String getCountry() {
-        return country;
-    }
+            public String getImage() {
+                return image;
+            }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+            public void setImage(String image) {
+                this.image = image;
+            }
 
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", country='" + country + '\'' +
-                ", image='" + image + '\'' +
-                '}';
-    }
-}
+            @Override
+            public String toString() {
+                return "Person{" +
+                        "id=" + id +
+                        ", name='" + name + '\'' +
+                        ", surname='" + surname + '\'' +
+                        ", email='" + email + '\'' +
+                        ", phone='" + phone + '\'' +
+                        ", address='" + address + '\'' +
+                        ", city='" + city + '\'' +
+                        ", country='" + country + '\'' +
+                        ", image='" + image + '\'' +
+                        '}';
+            }
+        }
